176bb112857a59ba6de2b6eecdfe9583560789f4
kikomn <chicamacaca@gmail.com>
2017-02-20 05:24:19 +0000

    Update switch.js

--- switch.js
+++ switch.js
@@ -1,4 +1,4 @@
-javascript:function htmlreplace(a,b,element){
-    if(!element)element=document.body;var nodes=element.childNodes;for(var n=0;n<nodes.length;n++)
-    {if(nodes[n].nodeType==Node.TEXT_NODE){nodes[n].textContent=nodes[n].textContent.replace(new RegExp(a,'gi'),b);}
-    else{htmlreplace(a,b,nodes[n]);}}}htmlreplace('black','green');
+function htmlreplace(a,b,element){
+    if(!element)element=document.body;var nodes=element.childNodes;for(var n=0;n<nodes.length;n++){
+    if(nodes[n].nodeType==Node.TEXT_NODE){nodes[n].textContent=nodes[n].textContent.replace(new RegExp(a,'gi'),b);}
+    else{htmlreplace(a,b,nodes[n]);}}}htmlreplace('black','green');
